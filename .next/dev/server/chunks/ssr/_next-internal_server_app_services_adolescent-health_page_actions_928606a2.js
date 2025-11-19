0c4a02fd41f669fd83c566af4d491e7c4ce8fc8d
MACBOOK PRO <bientobienvenu@gmail.com>
2025-11-19 12:45:33 +0300

    Remove node_modules and build files

--- .next/dev/server/chunks/ssr/_next-internal_server_app_services_adolescent-health_page_actions_928606a2.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/services/adolescent-health/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_services_adolescent-health_page_actions_928606a2.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mnCapdBF2tv4Xe1o+oobN5Nt/DJvlpXiISRj9jFE0IM=\"\n}"
